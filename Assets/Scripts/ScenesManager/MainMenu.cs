03a168ed9af07f72bced6be6039d9b1b6f5b9426
Santiacosta74 <santiagomagin.acosta@gmail.com>
2025-05-09 20:54:09 -0300

    Changes in UI scale

--- Assets/Scripts/ScenesManager/MainMenu.cs
+++ Assets/Scripts/ScenesManager/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("MainScene");
     }
 
     public void OpenSettings()
