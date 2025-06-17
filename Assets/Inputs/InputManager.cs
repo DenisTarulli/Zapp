26d49465ae82aa9bf0691e29cfb92e9322092053
IgnacioMartinezInfante <145930821+IgnacioMartinezInfante@users.noreply.github.com>
2025-06-17 16:17:38 -0300

    movimiento del mouse más corto para que te lo tome

--- Assets/Inputs/InputManager.cs
+++ Assets/Inputs/InputManager.cs
@@ -7,47 +7,50 @@ public class InputManager : Singleton<InputManager>
     #region Events
     public delegate void StartTouch(Vector2 position, float time);
     public event StartTouch OnStartTouch;
+
     public delegate void EndTouch(Vector2 position, float time);
     public event EndTouch OnEndTouch;
     #endregion
 
     private TouchControls playerControls;
-    private Camera mainCamera;
 
     private void Awake()
     {
         playerControls = new TouchControls();
-        mainCamera = Camera.main;
     }
 
     private void OnEnable()
     {
         playerControls.Enable();
+        playerControls.Touch.PrimaryContact.started += StartTouchPrimary;
+        playerControls.Touch.PrimaryContact.canceled += EndTouchPrimary;
     }
 
     private void OnDisable()
     {
+        playerControls.Touch.PrimaryContact.started -= StartTouchPrimary;
+        playerControls.Touch.PrimaryContact.canceled -= EndTouchPrimary;
         playerControls.Disable();
-    }
-
-    private void Start()
-    {
-        playerControls.Touch.PrimaryContact.started += ctx => StartTouchPrimary(ctx);
-        playerControls.Touch.PrimaryContact.canceled += ctx => EndTouchPrimary(ctx);
     }
 
     private void StartTouchPrimary(InputAction.CallbackContext context)
     {
-        OnStartTouch?.Invoke(Utils.ScreenToWorld(mainCamera, playerControls.Touch.PrimaryPosition.ReadValue<Vector2>()), (float)context.startTime);
+        Vector2 position = playerControls.Touch.PrimaryPosition.ReadValue<Vector2>();
+        float time = (float)context.startTime;
+
+        OnStartTouch?.Invoke(position, time);
     }
 
     private void EndTouchPrimary(InputAction.CallbackContext context)
     {
-        OnEndTouch?.Invoke(Utils.ScreenToWorld(mainCamera, playerControls.Touch.PrimaryPosition.ReadValue<Vector2>()), (float)context.time);
+        Vector2 position = playerControls.Touch.PrimaryPosition.ReadValue<Vector2>();
+        float time = (float)context.time;
+
+        OnEndTouch?.Invoke(position, time);
     }
 
     public Vector2 PrimaryPosition()
     {
-        return Utils.ScreenToWorld(mainCamera, playerControls.Touch.PrimaryPosition.ReadValue<Vector2>());
+        return playerControls.Touch.PrimaryPosition.ReadValue<Vector2>();
     }
 }
