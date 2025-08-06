d8c8e78f36492dd8e009effd400c39f14d737e15
IgnacioMartinezInfante <145930821+IgnacioMartinezInfante@users.noreply.github.com>
2025-08-06 03:09:35 -0300

    retoquesiños

--- Assets/Scripts/Objects/Obstacle.cs
+++ Assets/Scripts/Objects/Obstacle.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [Header("Movimiento")]
+    public bool moves = true; // Esto aparece en el inspector
 
     private void Update()
     {
+        if (!moves) return; // Si no se mueve, salimos
         transform.position -= new Vector3(0f, 0f, GameManager.Instance.scrollingSpeed * Time.deltaTime);
     }
 
